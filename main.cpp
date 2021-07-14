7cebe4df2f1dd7c5b34940365ba2d8f3c96329ec
HELLOMOONGYU <dlansrb58008928@gmail.com>
2021-07-14 09:10:44 -0400

    Add add-nbo

--- main.cpp
+++ main.cpp
@@ -9,11 +9,13 @@ int main(int argc, char *argv[])
     FILE *p1 = fopen(argv[1],"rb"); //thousand.bin
     fread(buffer,sizeof(buffer),1,p1);
     u_int32_t n1 = ntohl(buffer[0]);
+    fclose(p1);
 
     FILE *p2 = fopen(argv[2],"rb"); //five-hundred.bin
     fread(buffer,sizeof(buffer),1,p2);
     u_int32_t n2 = ntohl(buffer[0]);
     u_int32_t sum = n1+n2;
+    fclose(p2);
 
     printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n",n1,n1,n2,n2,sum,sum);
 }
